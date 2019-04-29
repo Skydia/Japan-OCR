7335c6f87eac30537f3baa975de7988002e45f34
Diemas Aksya <dimas.tauruzz@gmail.com>
2019-04-29 16:29:20 +0700

    Added files

---
+++ Japanese OCR/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tesseract;
+
+namespace Japanese_OCR
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openfile = new OpenFileDialog();
+            if (openfile.ShowDialog() == DialogResult.OK)
+            {
+                Bitmap img = new Bitmap(openfile.FileName);
+                TesseractEngine ocr = new TesseractEngine("./tessdata", "jpn", EngineMode.Default);
+                Page page = ocr.Process(img, PageSegMode.Auto);
+                textBox1.Text = page.GetText();
+            }
+        }
+    }
+}
